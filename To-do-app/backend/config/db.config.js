95a9fa6dacdcbcecb4935c9b3610be20479ce459
Syed Mohamad Arif <syedarifjr@gmail.com>
2024-04-09 14:05:54 +0800

    add docker

--- To-do-app/backend/config/db.config.js
+++ To-do-app/backend/config/db.config.js
@@ -4,7 +4,7 @@ const pool = createPool({
   host: process.env.MYSQL_HOST,
   port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
+  password: process.env.MYSQL_ROOT_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   connectionLimit: 10,
   connectTimeout: 30000,
